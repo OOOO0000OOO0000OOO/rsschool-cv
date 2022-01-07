ccf9894f4c42b5a960767638f69eb619671298fb
k0ntinuum <st088265@student.spbu.ru>
2022-01-07 12:21:03 +0300

    feat: add burger menu; make page responsive

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ button.addEventListener('click', () => {
   button.classList.toggle('active');
   list.style.maxHeight = (list.style.maxHeight) ? null : '150px';
 });
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+const navlist = document.querySelectorAll('.nav-link');
+
+[burger, ...navlist].forEach(item => {
+  item.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+  });
+});
